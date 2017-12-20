81b3b5fbfc715aa894a25332184310bc65976fdd
achr3f <mansouri.achref@gmail.com>
2017-12-20 11:29:18 +0100

    add a http  get request to communicate with a rails api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  company: 'any';
+  isValid: boolean;
+  options = { key: '' };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http, public alertCtrl: AlertController) {
+  }
+
+  public showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Clé invalide',
+      subTitle: 'clé invalide ! merci de conntacter l\'admin ',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  public getCompany() {
+    this.http.get('api/v1/mobile/company?key='+this.options.key).map(res => res.json()).subscribe(
+      data => {
+        this.isValid = true
+        this.company =  data.data.companies[0];
+
+     },
+     err => {
+       this.isValid = false
+       this.showAlert()
+       console.log( err );
+     }
+   );
 
   }
+
+
 
 }
